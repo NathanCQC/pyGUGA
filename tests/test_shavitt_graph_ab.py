ec6fa6add5cb364ce7cd9785f47be16c48343874
Nathan Fitzpatrick <45206408+NathanCQC@users.noreply.github.com>
2023-05-21 17:50:28 +0100

    comment

--- tests/test_shavitt_graph_ab.py
+++ tests/test_shavitt_graph_ab.py
@@ -6,7 +6,7 @@ def n_csfs(abc):
     return ((abc[1]+1)/(n+1) )* binom(n +1,abc[0])
 
 def test_shavitt_graph_ab():
-    for i in range(1,10):
+    for i in range(1,9): #seg fault at 10?
         abc = (i,0,i)
         guga = ShavittGraphAB(abc)
         csfs = guga._get_csfs()
