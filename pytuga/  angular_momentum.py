bee2065db69c9bacf3f0ce3138529704bdfe2c8a
Nathan Fitzpatrick <45206408+NathanCQC@users.noreply.github.com>
2025-01-14 10:04:10 +0000

    base setup

--- pytuga/  angular_momentum.py
+++
@@ -1,7 +0,0 @@
-from sympy import S
-from sympy.physics.wigner import clebsch_gordan
-
-
-
-# if __name__ == '__main__':
-#     pass
--- pytuga/__init__.py
+++
@@ -1,5 +0,0 @@
-from .guga_graph import ShavittGraph, ShavittGraphAB
-from .guga_state import TUGAStateAB
-from .guga_operator import TUGAOperatorAB
-
-from .angular_momentum import spin_coupling_tensor, spin_coupling_sub, spin_coupling_add
